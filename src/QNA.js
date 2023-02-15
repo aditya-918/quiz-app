37e662926a6b7638d89bcd3fffa2dfa16ddad30e
AyushDubey-maker <ayushdubey2611@gmail.com>
2023-02-16 00:22:26 +0530

    add q&a section updated

--- src/QNA.js
+++ src/QNA.js
@@ -4,20 +4,33 @@ import { db } from './firebase'
 
 const QNA = () => {
   const [question,setQuestion]=useState('');
-  const [answer,setAnswer]=useState('');
+  const [answerA,setAnswerA]=useState('');
+  const [answerB,setAnswerB]=useState('');
+  const [answerC,setAnswerC]=useState('');
+  const [answerD,setAnswerD]=useState('');
   const [section,setSection]=useState('');
+  const [correctAns,setCorrectAns]=useState('');
+
   function submitQA(){
-    if(question!=='' && answer!=='' && section!==""){
+    if(question!=='' && answerA!=='' && answerB!=='' && answerC!=='' && answerD!=='' && section!=="" && correctAns!==''){
     db.collection('question-answers').add({
       question:question,
-      answer:answer,
+      answerA:answerA,
+      answerB:answerB,
+      answerC:answerC,
+      answerD:answerD,
       section:section,
+      correctAns:correctAns
       
     })
     alert('Question added to the List')
     setQuestion('')
-    setAnswer('')
+    setAnswerA('')
+    setAnswerB('')
+    setAnswerC('')
+    setAnswerD('')
     setSection('')
+    setCorrectAns('')
   }else{
     alert('Fill all the fields');
   }
@@ -29,10 +42,15 @@ const QNA = () => {
         <div className="input_qna">
        
         <input className="add_q" placeholder="Add Questions" value={question} onChange={event=> setQuestion(event.target.value)}/>
-     
-        <input className="add_a" placeholder="Add Answer" value={answer} onChange={event=> setAnswer(event.target.value)}/>
+        <div>
+        <input className="add_a" placeholder="Option A Answer" value={answerA} onChange={event=> setAnswerA(event.target.value)}/>
+        <input className="add_a" placeholder="Option B Answer" value={answerB} onChange={event=> setAnswerB(event.target.value)}/>
+        <input className="add_a" placeholder="Option C Answer" value={answerC} onChange={event=> setAnswerC(event.target.value)}/>
+        <input className="add_a" placeholder="Option D Answer" value={answerD} onChange={event=> setAnswerD(event.target.value)}/>
+        </div>
         {/* <button type="Submit">Add Another Question  </button> */}
         {/* <label for="cars">Choose a category:</label> */}
+        <div>
   <select className="select_sections" id="cars" value={section} onChange={event=> setSection(event.target.value)}>
           <option value="">Select Section</option>
           <option value="ancient">Ancient</option>
@@ -40,6 +58,20 @@ const QNA = () => {
           <option value="ca">CA</option>
           <option value="polity">Polity</option>
   </select>
+
+  <select className="select_sections" id="cars" value={correctAns} onChange={event=> setCorrectAns(event.target.value)}>
+          <option value="">Correct Answer</option>
+          {answerA!=='' && answerB!=='' && answerC!=='' && answerD!=='' &&(
+          <>
+          
+          <option value={answerA}>{answerA}</option>
+          <option value={answerB}>{answerB}</option>
+          <option value={answerC}>{answerC}</option>
+          <option value={answerD}>{answerD}</option>
+          </>
+          )}
+  </select>
+  </div>
   <br />
         </div>
         <button className="qna_submit" onClick={submitQA}>Submit</button>
